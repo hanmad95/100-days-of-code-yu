7b2fbe5fbf2914518f6613f916d5bd6ffb8cd232
Madlinski <hannesmadl95@gmail.com>
2023-03-05 18:25:38 +0100

    Create day6.py

---
+++ 100_days_of_code/day6/day6.py
@@ -0,0 +1,14 @@
+# =====================================================================
+# Day  Challange: Reeborgs World
+# =====================================================================
+# [Task]: Escape the maze
+#
+
+# =====================================================================
+# Main
+# =====================================================================
+
+# -> use predefined functions within reeborgs world
+# -> make sure the robot goes to a position that it has a wall
+#    on his right side
+# -> follow allong the wall on the right side until reaching goal
